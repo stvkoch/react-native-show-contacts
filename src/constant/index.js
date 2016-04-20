d699c739ac67326d2c70cb9027d6bc44ec0e74d4
Steven Koch <steven.koch@fixeads.com>
2016-04-20 19:41:46 +0100

    base

---
+++ src/constant/index.js
@@ -0,0 +1,8 @@
+
+
+
+export const SEND_CONTACT = 'SEND_CONTACT';
+export const READ_CONTACT = 'READ_CONTACT';
+
+export const REQUEST_LOGIN = 'REQUEST_LOGIN';
+export const RECEIVE_LOGIN = 'RECEIVE_LOGIN';
