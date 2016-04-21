fddef593930bec10e95e9b5282d305cd3b388b20
Steven Koch <steven.koch@fixeads.com>
2016-04-21 19:36:11 +0100

    ad screen+

--- index.android.js
+++ index.android.js
@@ -1,11 +1,7 @@
 import React, {
   AppRegistry
 } from 'react-native';
-import StatusBarAndroid from 'react-native-android-statusbar';
 
 import Lellol from './src/lellol';
-import COLORS_SCHEMA from './src/colors';
-
-StatusBarAndroid.setHexColor(COLORS_SCHEMA.DARK_PRIMARY);
 
 AppRegistry.registerComponent('sendSMS', () => Lellol );
